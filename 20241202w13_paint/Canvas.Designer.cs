046ed719b6cc7e0eee68694206a4bd0e9ef29a41
TheNoobDice <alpha.thedice@gmail.com>
2024-12-02 15:02:47 +0800

    Created Paint Project and added basic functions.

---
+++ 20241202w13_paint/Canvas.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace _20241202w13_paint
+{
+    partial class frmCanvas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbxMain = new System.Windows.Forms.PictureBox();
+            this.dlgOpenJPG = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsslFileName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dlgSaveJPG = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMain)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 36);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNew,
+            this.tsmiOpen,
+            this.tsmiSaveAs,
+            this.toolStripSeparator1,
+            this.tsmiQuit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // tsmiNew
+            // 
+            this.tsmiNew.Name = "tsmiNew";
+            this.tsmiNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.tsmiNew.Size = new System.Drawing.Size(270, 34);
+            this.tsmiNew.Text = "New";
+            this.tsmiNew.Click += new System.EventHandler(this.tsmiNew_Click);
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.tsmiOpen.Size = new System.Drawing.Size(270, 34);
+            this.tsmiOpen.Text = "Open";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // tsmiSaveAs
+            // 
+            this.tsmiSaveAs.Name = "tsmiSaveAs";
+            this.tsmiSaveAs.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.tsmiSaveAs.Size = new System.Drawing.Size(270, 34);
+            this.tsmiSaveAs.Text = "Save as...";
+            this.tsmiSaveAs.Click += new System.EventHandler(this.tsmiSaveAs_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(267, 6);
+            // 
+            // tsmiQuit
+            // 
+            this.tsmiQuit.Name = "tsmiQuit";
+            this.tsmiQuit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.tsmiQuit.Size = new System.Drawing.Size(270, 34);
+            this.tsmiQuit.Text = "Quit";
+            this.tsmiQuit.Click += new System.EventHandler(this.tsmiQuit_Click);
+            // 
+            // pbxMain
+            // 
+            this.pbxMain.Location = new System.Drawing.Point(12, 36);
+            this.pbxMain.Name = "pbxMain";
+            this.pbxMain.Size = new System.Drawing.Size(100, 50);
+            this.pbxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxMain.TabIndex = 1;
+            this.pbxMain.TabStop = false;
+            this.pbxMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbxMain_MouseDown);
+            this.pbxMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxMain_MouseMove);
+            // 
+            // dlgOpenJPG
+            // 
+            this.dlgOpenJPG.Filter = "JPG|*.jpg";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslFileName});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 418);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 32);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsslFileName
+            // 
+            this.tsslFileName.Name = "tsslFileName";
+            this.tsslFileName.Size = new System.Drawing.Size(90, 25);
+            this.tsslFileName.Text = "File Name";
+            // 
+            // dlgSaveJPG
+            // 
+            this.dlgSaveJPG.Filter = "JPG|*.jpg";
+            // 
+            // frmCanvas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pbxMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmCanvas";
+            this.Text = "Canvas";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmCanvas_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMain)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNew;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem tsmiQuit;
+        private System.Windows.Forms.PictureBox pbxMain;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.OpenFileDialog dlgOpenJPG;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsslFileName;
+        private System.Windows.Forms.SaveFileDialog dlgSaveJPG;
+    }
+}
+
---
+++ 20241202w13_paint/Canvas.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20241202w13_paint
+{
+    public partial class frmCanvas : Form
+    {
+        private void newCanvas()
+        {
+            pbxMain.Image = new Bitmap(800, 600);
+            Graphics graphics = Graphics.FromImage(pbxMain.Image);
+            graphics.Clear(Color.White);
+            /* ezonesoft:
+             * 我們先建置一個 800 x 600 點的點陣圖物件作為 pbxMain 的 Image。
+             * 接著宣告一個稱為 graphics 的繪圖物件(Graphics)，
+             * 此物件來自於(也就是會執行繪圖動作於)pictureBox1.Image，
+             * 最後使用 graphics 將 Image 清除為白底(背景色)的狀況。
+             */
+        }
+
+    public frmCanvas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCanvas_Load(object sender, EventArgs e)
+        {
+            newCanvas();
+        }
+
+        int x0, y0;
+        private void pbxMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            x0 = e.X;
+            y0 = e.Y;
+        }
+
+        private void pbxMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Graphics graphics = Graphics.FromImage(pbxMain.Image);
+                graphics.DrawLine(Pens.Black, x0, y0, e.X, e.Y);
+                x0 = e.X;
+                y0 = e.Y;
+                pbxMain.Refresh();
+            }
+        }
+
+        private void tsmiNew_Click(object sender, EventArgs e)
+        {
+            newCanvas();
+
+            // frmCanvas_Load(sender, e)
+            /* ezonesoft:
+             * 是不是與前一單元的 SizeImage 等自訂副程式用法很像？
+             * 就是執行到此時去呼叫『開新檔案』的程式，請記得小括號內的參數必須照寫，
+             * 但物件型態名稱(object 與 EventArgs)則必須刪掉。
+             * 這也告訴我們其實事件副程式與一般自訂副程式沒有甚麼不同，也可以直接呼叫的 ！
+             */
+
+        }
+
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            string currentFileName = dlgOpenJPG.FileName;
+
+            pbxMain.Load(currentFileName);
+            tsslFileName.Text = currentFileName;
+
+            /*
+            currentZoomLevel = 5;
+            ResizePictureBox();
+            CenterPictureBox();
+            */
+        }
+
+        private void tsmiSaveAs_Click(object sender, EventArgs e)
+        {
+            if (dlgSaveJPG.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(pbxMain.Image);
+                bmp.Save(dlgSaveJPG.FileName);
+                /* ezonesoft:
+                 * 上述程式中的 Bitmap bmp = new Bitmap(pictureBox1.Image)
+                 * 是宣告一個稱為 bmp 的Bitmap(點陣圖)物件，
+                 * 內容與 pictureBox1.Image 相同；
+                 * 接下來將此物件儲存(Save)為選定檔名 saveFileDialog1.FileName。
+                 * 此動作似乎有些多餘，
+                 * 但是直接用 pictureBox1.Image.Save 這樣的語法卻會產生「GDI+泛型錯誤」，
+                 * 同樣.NET Framework 版本的 VB 這樣寫卻是可以的！
+                 * 只能說是 C#語法比較嚴謹吧？
+                 */
+            }
+        }
+
+        private void tsmiQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
